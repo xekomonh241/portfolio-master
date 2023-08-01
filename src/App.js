7f913af599fa1fb6fd47d9feb59bf0b2e8356820
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-08-01 23:49:47 +0700

    startup

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Features from "./components/features/Features";
 
 function App() {
   return (
-    <div className="w-full h-auto bg-bodyColor text-lightText">
-      <div className="max-w-screen-2xl mx-auto px-16">
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
         <Navbar />
+      <div className="max-w-screen-2xl mx-auto">
         <Banner />
         <Features />
       </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import bannerImg from "./images/banner.jpeg";
+import bannerImg from "./images/banner.JPEG";
 import solshield from "./images/projects/solshield.png";
 import ecshop from "./images/projects/ecshop.png";
 import portfolio from "./images/projects/portfolio.png";
@@ -6,8 +6,8 @@ import contactImg from "./images/contact/contactImg.jpg"
 
 export {
     bannerImg,
-    ecshop,
+    contactImg,
     portfolio,
     ecshop,
-    solshield,
+    solshield
 };
