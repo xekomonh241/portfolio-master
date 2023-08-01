82f31d4d7d4e82d11fac2d5135982cb082168ede
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-08-01 15:11:25 +0700

    startup

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import bannerImg from "./images/banner.jpeg";
+import solshield from "./images/projects/solshield.png";
+import ecshop from "./images/projects/ecshop.png";
+import portfolio from "./images/projects/portfolio.png";
+import contactImg from "./images/contact/contactImg.jpg"
+
+export {
+    bannerImg,
+    ecshop,
+    portfolio,
+    ecshop,
+    solshield,
+};
