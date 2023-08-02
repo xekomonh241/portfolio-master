fda599f888998812a595a370a74ca8b4228d9d7b
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-08-02 16:00:56 +0700

    projects done

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Title from "../layouts/Title";
+import ProjectsCard from "./ProjectsCard";
+import { solshield, ecshop, portfolio } from "../../assets/index";
+
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <Title title="VISIT MY PROJECTS" des="My Projects" />
+      </div>
+      <div className="grid grid-cols-3 gap-14">
+        <ProjectsCard
+          title="SOCIALFI"
+          des="SolShield is the ideal solution for electronic signature (e-signatures) technology and digitizing contract.
+            SolShield software allows parties to sign contracts and other documents electronically on Solana rather than signing 
+            them with pen and paper. This eliminates the need for parties signing a contract to be physically present at the same location."
+          src={solshield}
+          srcCode="https://github.com/xekomonh241/solshield"
+          linkGlobe="https://solshield.onrender.com/"
+        />
+        <ProjectsCard
+          title="E-commerce Website"
+          des=" EcShop is an e-commerce website built using the PHP programming language. 
+            EcShop has almost all the features of an e-commerce website today."
+          src={ecshop}
+          srcCode="https://github.com/xekomonh241/ecsgu/tree/main"
+          linkGlobe=""
+        />
+        <ProjectsCard
+          title="Personal Portfolio"
+          des=" This is a website that aggregates information about myself with projects 
+            I have done along with skills and resumes"
+          src={portfolio}
+          srcCode="https://github.com/xekomonh241/portfolio"
+          linkGlobe="https://portfolio-ilaw.onrender.com/"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
