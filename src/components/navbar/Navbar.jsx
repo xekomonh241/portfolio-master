a9183dcb2a566652e9c5fb1e0e8508c26ff6ad42
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-08-02 22:30:58 +0700

    fix navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import { Link } from "react-scroll"
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+import { FaFacebookF, FaTwitter, FaTelegram, FaInstagram } from "react-icons/fa";
 import { navLinksdata } from "../../constants";
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div className="text-3xl pl-10">Portfolio.</div>
       <div>
-        <ul className="flex items-center gap-10">
+        <ul className="hidden mr-10 mdl:inline-flex items-center gap-6 lg:gap-10">
           {navLinksdata.map(({ id, title, link }) => (
             <li
               className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
@@ -25,6 +30,72 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <span
+          onClick={() => setShowMenu(!showMenu)}
+          className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
+        >
+          <FiMenu />
+        </span>
+        {showMenu && (
+          <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
+            <div className="flex flex-col gap-8 py-2 relative">
+              <ul className="flex flex-col gap-4">
+                {navLinksdata.map((item) => (
+                  <li
+                    key={item._id}
+                    className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+                  >
+                    <Link
+                      onClick={() => setShowMenu(false)}
+                      activeClass="active"
+                      to={item.link}
+                      spy={true}
+                      smooth={true}
+                      offset={-70}
+                      duration={500}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex flex-col gap-4">
+                <h2 className="text-base uppercase font-titleFont mb-4">
+                  Find me in
+                </h2>
+                <div className="flex gap-4">
+                  <a
+                    href="https://www.facebook.com/ducmanh1101/"
+                    className="bannerIcon"
+                  >
+                    <FaFacebookF />
+                  </a>
+                  <a
+                    href="https://twitter.com/NguyncM10987225"
+                    className="bannerIcon"
+                  >
+                    <FaTwitter />
+                  </a>
+                  <a href="https://t.me/xekomonh" className="bannerIcon">
+                    <FaTelegram />
+                  </a>
+                  <a
+                    href="https://www.instagram.com/duc.manguyn/"
+                    className="bannerIcon"
+                  >
+                    <FaInstagram />
+                  </a>
+                </div>
+              </div>
+              <span
+                onClick={() => setShowMenu(false)}
+                className="absolute top-4 right-4 text-gray-400 hover:text-designColor duration-300 text-2xl cursor-pointer"
+              >
+                <MdClose />
+              </span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
