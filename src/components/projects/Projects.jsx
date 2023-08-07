ef90ed7a78c17069be52b4291f79cddfb5e29284
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-08-07 14:36:00 +0700

    complete project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Title from "../layouts/Title";
 import ProjectsCard from "./ProjectsCard";
 import { solshield, ecshop, portfolio } from "../../assets/index";
+import TechSolShield from "./TechSolShield";
+import TechEcshop from "./TechEcshop";
+import TechPortfolio from "./TechPortfolio";
 
 const Projects = () => {
   return (
@@ -14,28 +17,34 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         <ProjectsCard
+          id="01"
           title="SOCIALFI"
           des="SolShield is the ideal solution for electronic signature (e-signature) technology and contract digitization. 
           It allows parties to sign them electronically on Solana. SolShield won first prize U20 Track in Solana Vietnam conding camp season 2."
           src={solshield}
           srcCode="https://github.com/xekomonh241/solshield"
           linkGlobe="https://solshield.onrender.com/"
+          tech=<TechSolShield />
         />
         <ProjectsCard
+          id="02"
           title="E-commerce Website"
           des=" EcShop is an e-commerce website written in PHP. EcShop has almost all of the features of a modern 
           e- commerce website"
           src={ecshop}
           srcCode="https://github.com/xekomonh241/ecsgu/tree/main"
           linkGlobe=""
+          tech=<TechEcshop />
         />
         <ProjectsCard
+          id="03"
           title="Personal Portfolio"
           des=" This is a website that collects information about myself, including projects
            I have worked on, as well as skills and resumes"
           src={portfolio}
           srcCode="https://github.com/xekomonh241/portfolio"
           linkGlobe="https://portfolio-ilaw.onrender.com/"
+          tech=<TechPortfolio />
         />
       </div>
     </section>
--- src/components/projects/ProjectsCard.jsx
+++ src/components/projects/ProjectsCard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { BsGithub } from "react-icons/bs"
 import { FaGlobe } from "react-icons/fa"
-import Technologies from "./Technologies";
 
-const ProjectsCard = ({ title, des, src, srcCode, linkGlobe }) => {
+const ProjectsCard = ({ title, des, src, srcCode, linkGlobe, tech }) => {
   return (
     <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
       <div className="w-full h-auto overflow-hidden rounded-lg">
@@ -13,7 +12,7 @@ const ProjectsCard = ({ title, des, src, srcCode, linkGlobe }) => {
           alt="src"
         />
       </div>
-      <div className="w-full mt-5 flex flex-col gap-6">
+      <div className="w-full h-auto md:h-[250px] mdl:h-[200px] lgl:h-[180px] xl:h-[280px] mt-5 flex flex-col gap-6">
         <div>
           <div className="flex items-center justify-between">
             <h3 className="text-base uppercase text-designColor font-normal">
@@ -33,8 +32,8 @@ const ProjectsCard = ({ title, des, src, srcCode, linkGlobe }) => {
           </p>
         </div>
       </div>
-      <div className="w-full h-auto bottom-0">
-          <Technologies />
+      <div className="w-full h-auto ">
+          {tech}
       </div>
     </div>
   );
