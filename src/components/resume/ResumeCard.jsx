efb2fd4ee014659d25cdbdff5e53d638cba757fd
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-08-08 00:22:56 +0700

    prepare deploy

--- src/components/resume/ResumeCard.jsx
+++ src/components/resume/ResumeCard.jsx
@@ -11,7 +11,7 @@ const ResumeCard = ({title, subTitle, result, des}) => {
       <div className="w-full bg-black bg-opacity-20 hover:bg-opacity-30 duration-300  rounded-lg p-4 lgl:px-10 flex flex-col justify-center gap-6 lgl:gap-10 shadow-shadowOne">
         <div className="flex flex-col lgl:flex-row justify-between gap-4 lgl:gap-0 lgl:items-center">
           <div>
-            <h3 className="text-2xl md:text-3xl font-semibold group-hover:text-white duration-300">
+            <h3 className="text-2xl text-justify md:text-3xl font-semibold group-hover:text-white duration-300">
               {title}
             </h3>
             <p className="text-sm mt-2 text-gray-400 group-hover:text-white duration-300">
@@ -24,7 +24,7 @@ const ResumeCard = ({title, subTitle, result, des}) => {
             </p>
           </div>
         </div>
-        <p className="text-sm md:text-base font-medium text-gray-400 group-hover:text-gray-300 duration-300">
+        <p className="text-sm md:text-base text-justify font-medium text-gray-400 group-hover:text-gray-300 duration-300">
           {des}
         </p>
       </div>
