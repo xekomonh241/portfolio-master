ac2b02d0b61e3e9b4bc9e23472d4854e8b6bfe3c
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-08-08 20:37:23 +0700

    prepare deploy

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import bannerImg from "./images/banner.JPEG";
 import solshield from "./images/projects/solshield.png";
 import ecshop from "./images/projects/ecshop.png";
 import portfolio from "./images/projects/portfolio.png";
-import contactImg from "./images/contact/contactImg.JPG"
+import contactImg from "./images/contact/contactImg.jpg"
 
 export {
     bannerImg,
